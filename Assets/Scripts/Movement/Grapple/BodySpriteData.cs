a0631468c2b98259fd50b8597c5854a83c0f3ea9
Elijah Pennington <elijahmpennington@gmail.com.com>
2024-03-05 15:50:24 -0600

    Add Character Sprite to Assets

---
+++ Assets/Scripts/Movement/Grapple/BodySpriteData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BodySpriteData", menuName = "BodySpriteData", order = 0)]
+public class BodySpriteData : ScriptableObject
+{
+    public Sprite Up;
+    public Sprite Straight;
+    public Sprite Down;
+}
